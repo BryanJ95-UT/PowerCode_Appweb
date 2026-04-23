8d8c6a71829ac15db49db7aaaf1bda0ee38bd24f
alancruzbaltazar <240516@utxicotepec.edu.mx>
2026-04-22 21:13:19 -0600

    Backend: API inicial con rutas de auth

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,20 @@ app.get('/api', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor activo en puerto 3000');
 });
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API Power Code funcionando ');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
