0dc510f34dcfb4419573742eca35eba7b86131d8
alancruzbaltazar <240516@utxicotepec.edu.mx>
2026-04-22 22:28:15 -0600

    nuevos cambios

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-app.get('/api', (req, res) => {
-  res.json({ mensaje: 'Backend funcionando correctamente' });
-});
-
-app.listen(3000, () => {
-  console.log('Servidor activo en puerto 3000');
-});
-
 import express from 'express';
 import cors from 'cors';
+
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/users.routes.js';
+import membershipRoutes from './routes/memberships.routes.js';
 
 const app = express();
 const PORT = 3000;
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API Power Code funcionando ');
+// Ruta de prueba
+app.get('/api', (req, res) => {
+  res.json({ mensaje: 'Backend funcionando correctamente' });
 });
 
+app.get('/', (req, res) => {
+  res.send('API Power Code funcionando');
+});
+
+// Rutas
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/memberships', membershipRoutes);
+
+// Servidor (SOLO UNA VEZ)
 app.listen(PORT, () => {
   console.log(`Servidor en http://localhost:${PORT}`);
 });
